418595cbae75ef5a215c611e4528ec0f0dac5444
Josephat Mwagongo <jmwanguwe3@gmail.com>
2025-07-12 17:48:47 +0300

    Fix Supabase URL typo

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://myaltytkgiejtkvvsesejd.supabase.co'
+const supabaseUrl = 'https://myaltykgiejtkvvsesjd.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15YWx0eWtnaWVqdGt2dnNlc2pkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwNjYwMjAsImV4cCI6MjA2NzY0MjAyMH0.I9JtpCVyBNstVk27bvTV2m9ngyujdFPoQsHC6csUPfg'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
